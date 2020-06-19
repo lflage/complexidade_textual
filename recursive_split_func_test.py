48e7687c3e7d581458512511f4a65a8abae895b8
Lucas Fonseca <lucasflage@hotmail.com>
2020-06-19 16:48:11 -0300

    Estrutura de listas para conter a árvore sintática

--- recursive_split_func_test.py
+++ recursive_split_func_test.py
@@ -10,15 +10,16 @@ import re
 def recursive_split(text):
     eq_sub = re.compile(r'^=', flags=re.M)
     current_level = []
-    if re.search(r'=',text):
-        contents = re.split(r'\n(?=\w)',text)
-        for i in contents:
+    contents = re.split(r'\n(?=\w)',text)
+    for i in contents:
+        if re.search('=', i):    
             nodes = re.split(r'(^\w.+\n)', i)
             equal_remove = re.sub(eq_sub, '', nodes[2])
             current_level.append([nodes[1], recursive_split(equal_remove)])
-        return current_level
-    else:   
-        return re.split(r'\n', text)
+        else:
+            current_level.append([i])
+    return current_level
+
       
 text = 'SOURCE: Running text\n1. A menina que brincava no parque perdeu a boneca\nA1\nUTT:cl(fcl)\nS:g(np)\n=D:pron(det "o" <*> <artd> DET F S) A\n=H:n("menina" F S) menina\n=D:cl(fcl)\n==S:pron(indp "que" <rel> M S) que\n==P:v(fin "brincar" IMPF 3S IND VFIN) brincava\n==A:g(pp)\n===H:prp("em" <sam-> <right>) em\n===D:g(np)\n====D:pron(det "o" <-sam> <artd> DET M S) o\n====H:n("parque" M S) parque\nP:v(fin "perder" <fmc> PS 3S IND VFIN) perdeu\nOd:g(np)\n=D:pron(det "o" <artd> DET F S) a\n=H:n("boneca" F S) boneca'
 

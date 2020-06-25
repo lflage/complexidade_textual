f1db18bcfa56f2bcf6a1ec0fb8ccd028209486d6
Lucas Fonseca <lucasflage@hotmail.com>
2020-06-25 09:37:29 -0300

    crawler agora salva as redações parseadas em um arquivo _parsed_.txt

--- palavras_crawler.py
+++ palavras_crawler.py
@@ -1,47 +1,53 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-Este é um arquivo de script temporário.
+Crawler para receber os textos parseados pelo Parser Sintático Palavras
 """
 
-#import os, time
-#import complexidade_textual as ct
+import os, nltk
+import complexidade_textual as ct
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
-
-#files, paths = ct.corpus_reader("./corpora/Redações")
-
 # Setting up Firefox
-# Setting up Profile 
-
-"""
-profile = webdriver.FirefoxProfile()
-profile.set_preference("browser.download.folderList", 1)
-profile.set_preference("browser.download.manager.showWhenStarting", False)
-profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/html")
-profile.update_preferences()
-"""
-
 driver = webdriver.Firefox()
 driver.get("https://visl.sdu.dk/visl/pt/parsing/automatic/trees.php")
 assert "VISL - Tree structure" in driver.title
 
-#Textbox element that will receive the parsed text
-text_box = driver.find_element_by_name("text")
-text_box.clear()
-text_box.send_keys("A menina que brincava no parque perdeu a boneca")
+# Reading corpus
+corpus = ct.corpus_yeeter('./corpora/Redações')
 
-# Select vertical visualization from dropdown box
-visualization = Select(driver.find_element_by_name('visual'))
-visualization.select_by_visible_text("Source")
+# Sentence tokenizer
+sent_tokenizer = nltk.data.load('tokenizers/punkt/portuguese.pickle') 
 
-# Click to show parsed text
-exp_down = driver.find_element_by_name("go")
-exp_down.click()
 
-parsed_text = driver.find_element_by_tag_name('pre')
-print(parsed_text.text)
+for path, doc in corpus:
+    # Leitura do documento
+    doc.read()
+    redacao = doc.get_body()
+    
+    # Tratamento do path
+    caminho = os.path.splitext(path)
+    
+    parsed_sent_list = []
+    for sent in sent_tokenizer.tokenize(redacao):
+        #Textbox element that will receive the parsed text
+        text_box = driver.find_element_by_name("text")
+        text_box.clear()
+        text_box.send_keys(sent)
 
-# time.sleep(2)
+        # Select vertical visualization from dropdown box
+        visualization = Select(driver.find_element_by_name('visual'))
+        visualization.select_by_visible_text("Source")
+
+        # Click to show parsed text
+        exp_down = driver.find_element_by_name("go")
+        exp_down.click()
+
+        parsed_text = driver.find_element_by_tag_name('pre')
+        parsed_sent_list.append(parsed_text.text)
+        
+    with open ("%s_parsed_%s" % (caminho[0],'.txt'), 'w') as file:
+        file.write("\nsentence\n".join(parsed_sent_list))
+
+    sleep(1.5)

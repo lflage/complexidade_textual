1af23b31f280aa98a6c91abd014da90fc642c67c
Lucas Fonseca <lucasflage@hotmail.com>
2020-06-25 11:06:28 -0300

    crawler agora funciona por linha de comando

--- palavras_crawler.py
+++ palavras_crawler.py
@@ -3,11 +3,13 @@
 Crawler para receber os textos parseados pelo Parser Sintático Palavras
 """
 
-import os, nltk
+import os, nltk, sys
 import complexidade_textual as ct
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+
+#path_to_corpus = sys.argv[1]
 
 # Setting up Firefox
 driver = webdriver.Firefox()
@@ -15,6 +17,7 @@ driver.get("https://visl.sdu.dk/visl/pt/parsing/automatic/trees.php")
 assert "VISL - Tree structure" in driver.title
 
 # Reading corpus
+#corpus = ct.corpus_yeeter(path_to_corpus)
 corpus = ct.corpus_yeeter('./corpora/Redações')
 
 # Sentence tokenizer
@@ -31,22 +34,26 @@ for path, doc in corpus:
     
     parsed_sent_list = []
     for sent in sent_tokenizer.tokenize(redacao):
-        #Textbox element that will receive the parsed text
-        text_box = driver.find_element_by_name("text")
-        text_box.clear()
-        text_box.send_keys(sent)
+        try:
+            #Textbox element that will receive the parsed text
+            text_box = driver.find_element_by_name("text")
+            text_box.clear()
+            text_box.send_keys(sent)
 
-        # Select vertical visualization from dropdown box
-        visualization = Select(driver.find_element_by_name('visual'))
-        visualization.select_by_visible_text("Source")
+            # Select vertical visualization from dropdown box
+            visualization = Select(driver.find_element_by_name('visual'))
+            visualization.select_by_visible_text("Source")
 
-        # Click to show parsed text
-        exp_down = driver.find_element_by_name("go")
-        exp_down.click()
+            # Click to show parsed text
+            exp_down = driver.find_element_by_name("go")
+            exp_down.click()
 
-        parsed_text = driver.find_element_by_tag_name('pre')
-        parsed_sent_list.append(parsed_text.text)
-        
+            parsed_text = driver.find_element_by_tag_name('pre')
+            parsed_sent_list.append(parsed_text.text)
+        except:
+            print(path)
+            exit()
+            
     with open ("%s_parsed_%s" % (caminho[0],'.txt'), 'w') as file:
         file.write("\nsentence\n".join(parsed_sent_list))
 

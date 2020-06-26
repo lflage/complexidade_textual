a4f307df4fdff80d4a9b189f17164cc87cd1a06e
Lucas Fonseca <lucasflage@hotmail.com>
2020-06-26 18:44:33 -0300

    crawler ignora redações já parseadas e da um intervalo de 10min caso haja um erro

--- palavras_crawler.py
+++ palavras_crawler.py
@@ -9,22 +9,28 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
-path_to_corpus = sys.argv[1]
+# Reading corpus
+#path_to_corpus = sys.argv[1]
+path_to_corpus = './corpora/Redações'
+corpus = ct.corpus_yeeter(path_to_corpus)
+
+files_list = ct.all_fps(path_to_corpus)
+
 
 # Setting up Firefox
 driver = webdriver.Firefox()
 driver.get("https://visl.sdu.dk/visl/pt/parsing/automatic/trees.php")
 assert "VISL - Tree structure" in driver.title
 
-# Reading corpus
-corpus = ct.corpus_yeeter(path_to_corpus)
-#corpus = ct.corpus_yeeter('./corpora/Redações')
-
 # Sentence tokenizer
 sent_tokenizer = nltk.data.load('tokenizers/punkt/portuguese.pickle') 
 
 
 for path, doc in corpus:
+    # Checa se o arquivo já foi criado e pula para o próximo caso não
+    if ct.parsed_search(path, files_list):
+        print("\nskipped\n")
+        continue
     # Leitura do documento
     doc.read()
     redacao = doc.get_body()
@@ -52,7 +58,8 @@ for path, doc in corpus:
             parsed_sent_list.append(parsed_text.text)
         except:
             print(path)
-            exit()
+            sleep(600)
+            continue
             
     with open ("%s_parsed_%s" % (caminho[0],'.txt'), 'w') as file:
         file.write("\nsentence\n".join(parsed_sent_list))
